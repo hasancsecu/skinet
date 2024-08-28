cb1ae787405b624be736bbc14e4f3c11818eea58
Muhammad Hasan <tamanna.hasan@dohatec.com.bd>
2024-08-28 15:40:51 +0600

    get brands and types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,27 +6,45 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController: ControllerBase
+    public class ProductsController : ControllerBase
     {
         public IProductRepository Repo { get; }
         public ProductsController(IProductRepository repo)
         {
             Repo = repo;
-            
+
         }
         [HttpGet]
-        public async Task<IReadOnlyList<Product>> GetProducts() {
+        public async Task<IReadOnlyList<Product>> GetProducts()
+        {
             var products = await Repo.GetProductsAsync();
 
             return products;
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id) {
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
             var product = await Repo.GetProductByIdAsync(id);
 
             return product;
         }
-        
+
+        [HttpGet("brands")]
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrands()
+        {
+            var brands = await Repo.GetProductBrandsAsync();
+
+            return brands;
+        }
+        [HttpGet("types")]
+        public async Task<IReadOnlyList<ProductType>> GetProductTypes()
+        {
+            var types = await Repo.GetProductTypesAsync();
+
+            return types;
+        }
+
+
     }
 }
